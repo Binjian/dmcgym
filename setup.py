0868e726418fa9c9e7de246694dc4383085b4c87
Binjain Xin <binjian.xin@hotmail.com>
2024-06-18 09:05:25 +0800

    [init] gym->gymnasium, mujoco-py->mujoco

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     description=description,
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/ikostrikov/dmcgym',
+    url='https://github.com/binjian/dmcgym',
     author='Ilya Kostrikov',
     classifiers=[
         'Development Status :: 3 - Alpha',
